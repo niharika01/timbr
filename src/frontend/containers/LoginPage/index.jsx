413e4feafb05f06927cae9425dc60c8baf640318
Anvi Mehta <32017801+anvimehta@users.noreply.github.com>
2020-09-26 13:01:44 -0400

    Google and Facebook auth (#10)
    * inital google signin
    
    * google auth complete with firebase
    
    * inital google signin
    
    * google auth complete with firebase
    
    * removing  unneeded files
    
    * remove files
    
    * inital google signin
    
    * google auth complete with firebase
    
    * removing  unneeded files
    
    * inital google signin
    
    * google auth complete with firebase
    
    * remove files
    
    * changes
    
    * removing  weird errors
    
    * inital google signin
    
    * google auth complete with firebase
    
    * removing  unneeded files
    
    * inital google signin
    
    * google auth complete with firebase
    
    * remove files
    
    * inital google signin
    
    * google auth complete with firebase
    
    * removing  unneeded files
    
    * google auth complete with firebase
    
    * changes
    
    * removing  weird errors
    
    * added items to package-lock.json
    
    * inital google signin
    
    * google auth complete with firebase
    
    * removing  unneeded files
    
    * inital google signin
    
    * google auth complete with firebase
    
    * remove files
    
    * inital google signin
    
    * google auth complete with firebase
    
    * removing  unneeded files
    
    * google auth complete with firebase
    
    * changes
    
    * removing  weird errors
    
    * added items to package-lock.json
    
    * google auth complete with firebase
    
    * google auth complete with firebase
    
    * changes
    
    * removing merge conflict statements
    
    * facebook auth
    
    * merge changes
    
    * Fixes linting
    
    * facebook sign  in error log
    
    * removing white spaces causing lint errors
    
    * removing unused dependencies
    
    Co-authored-by: drewthoennes <dthoenne@purdue.edu>

--- src/frontend/containers/LoginPage/index.jsx
+++ src/frontend/containers/LoginPage/index.jsx
@@ -4,15 +4,71 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import firebase from 'firebase';
 import axios from 'axios';
 import map from '../../store/map';
 import './styles.scss';
 import history from '../../router/history';
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyAKPviI0W3v3WuBBxMeZblLcr_t7Y63VAs',
+  authDomain: 'timbr-cs407.firebaseapp.com',
+});
+
+function signInGoogle(event) {
+  // console.log(event);
+  event.preventDefault();
+  const prv = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(prv);
+  /*
+    .then((result) => {
+      const { user } = result;
+    });
+  */
+}
+
+function signInFacebook() {
+  const provider = new firebase.auth.FacebookAuthProvider();
+  // firebase.auth().signInWithPopup(provider).then((result) => {
+  firebase.auth().signInWithPopup(provider).then(() => {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    // const token = result.credential.accessToken;
+    // The signed-in user info.
+    // const { user } = result;
+    // ...
+    // }).catch((error) => {
+  }).catch(() => {
+    // Handle Errors here.
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
+    // The email of the user's account used.
+    // const { email } = error;
+    // The firebase.auth.AuthCredential type that was used.
+    // const { credential } = error;
+    // ...
+  });
+}
+
+function signOut() {
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+  }).catch(() => {
+    // An error happened.
+  });
+}
+
 class LoginPage extends React.Component {
   constructor() {
     super();
+    this.state = {
+      auth: false,
+    };
+  }
 
+  componentDidMount() {
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+      (user) => this.setState({ auth: !!user }),
+    );
     this.handleLogin = this.handleLogin.bind(this);
   }
 
@@ -37,27 +93,42 @@ class LoginPage extends React.Component {
   }
 
   render() {
+    const { auth } = this.state;
+
+    if (!auth) {
+      return (
+        <div>
+          <h1>Click below to sign-in</h1>
+
+          <h1>timbr Login Page!</h1>
+          <form id="register-form" onSubmit={this.handleLogin}>
+            <input
+              id="email"
+              type="text"
+              placeholder="Email"
+            />
+
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+            />
+
+            <button type="submit">Login</button>
+          </form>
+
+          <button type="button" onClick={signInGoogle}>SIGN IN WITH GOOGLE</button>
+          <button type="button" onClick={signInFacebook}>SIGN IN WITH FACEBOOK</button>
+        </div>
+      );
+    }
     return (
-      <div id="login-page">
-        <h1>timbr Login Page!</h1>
-        <form id="register-form" onSubmit={this.handleLogin}>
-          <input
-            id="email"
-            type="text"
-            placeholder="Email"
-          />
+      <div>
+        <h1>Click below to sign-out</h1>
+        <button type="button" onClick={signOut}>SIGN OUT</button>
 
-          <input
-            id="password"
-            type="password"
-            placeholder="Password"
-          />
-
-          <button type="submit">Login</button>
-        </form>
       </div>
     );
   }
 }
-
 export default connect(map)(withRouter(LoginPage));
