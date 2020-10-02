654fe168801010dce2b42d99f24829552ba42227
Tanya Khandelwal <tkhandel@purdue.edu>
2020-10-01 21:30:47 -0400

    Add users to the database on first login (#13)
    * Input Validation for sign up
    
    * Error handling with redirection not working
    
    * Adding users to database
    
    * Input Validation for sign up
    
    * Error handling with redirection not working
    
    * Adding users to database
    
    * Removing some irrelevant changes.

--- src/frontend/store/reducers/auth.js
+++ src/frontend/store/reducers/auth.js
@@ -3,52 +3,70 @@
 import { firebase, facebookAuthProvider, googleAuthProvider } from '../firebase/firebase';
 import AuthOptions from '../const';
 
+/* This method adds the current user to the database, if not already added. */
+function addToDatabase() {
+  const currUser = firebase.auth().currentUser;
+  if (currUser) {
+    firebase.database().ref(`users/${currUser.uid}`).once('value', (user) => {
+      if (!user.exists()) {
+        firebase.database().ref(`users/${currUser.uid}`).set({
+          /* We can store something else other than the email,
+            possibly the username. */
+          email: currUser.email,
+        });
+      }
+    });
+  }
+}
+
 /* This method uses firebase auth to create a new user. */
 function registerWithTimbr(credentials) {
-  // TODO: Firebase auth error handling.
   firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)
-    .then((() => {
+    .then(() => {
       console.log('User created!');
-    }))
-    .catch(((error) => {
+    })
+    .catch((error) => {
       console.error(error.message);
-    }));
+    });
 }
 
 /* This method uses firebase auth to sign in a user. */
 function loginWithTimbr(credentials) {
   // TODO: Firebase auth error handling.
   firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
-    .then((() => {
+    .then(() => {
       console.log('User signed in!');
-    }))
-    .catch(((error) => {
+      addToDatabase();
+    })
+    .catch((error) => {
       console.error(error.message);
-    }));
+    });
 }
 
 /* This function uses Firebase auth to sign in a user using Facebook. */
 function loginWithFacebook() {
   // TODO: Firebase auth error handling.
   firebase.auth().signInWithPopup(facebookAuthProvider)
-    .then((() => {
+    .then(() => {
       console.log('User logged in with Facebook!');
-    }))
-    .catch(((error) => {
+      addToDatabase();
+    })
+    .catch((error) => {
       console.error(error.message);
-    }));
+    });
 }
 
 /* This function uses firebase auth to sign in a user using Google. */
 function loginWithGoogle() {
   // TODO: Firebase auth error handling.
   firebase.auth().signInWithPopup(googleAuthProvider)
-    .then((() => {
+    .then(() => {
       console.log('User logged in with Google!');
-    }))
-    .catch(((error) => {
+      addToDatabase();
+    })
+    .catch((error) => {
       console.error(error.message);
-    }));
+    });
 }
 
 /* This function uses firebase auth to log out a user */
